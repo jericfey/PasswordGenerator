98d01c330e5a489a94dc50c9c3bbf22ac7f06762
Jeri Fey <jericfey@gmail.com>
2020-10-12 20:20:14 -0600

    added selection pools for requirements

--- script.js
+++ script.js
@@ -49,8 +49,19 @@ console.log(passwordOptions);
 //need a for loop to cycle inputPassLenth number of times
 for (i = 0; inputPassLength < i; i++){
  //Need to generate a random lowercase letter if selected as a requirement
-  // var random
+// var random = Math.random(0-inputPassLength)*1
+// var temp = options(random)
+//finalPassword.push(temp)
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// define pools of content to run randomizer against
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+var lowerCase = ['a','b','c','d','e','f','g,','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+
+var spchars = ['!','#','$','%','&','(',')','*','+',',','-','.',':',';','<','>','=','?','@','[',']','^','_','`','{','}','|','~'];
